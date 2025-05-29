1e8f7f1828de718d8be5cfde95f287349167dcab
Matthias Stahl <matthias.stahl87@web.de>
2025-05-29 14:16:16 +0200

    add gift article detection

--- subscriptionCookie.js
+++ subscriptionCookie.js
@@ -7,24 +7,31 @@
  * subscriptionCookie.isPlusSubscriber - Indicates if the user is a Plus subscriber.
 
  * isPlusSubscriber (boolean) - Indicates if the user is a Plus subscriber based on the 'accessInfo' cookie.
+ * isGiftArticle (boolean) - Indicates if the article was called as a gift article (gift token is not validated).
+ * isOpen (boolean) - Indicates if isPlusSubscriber or isGiftArticle is true.
  */
 
 const cookies = document.cookie;
 
 const accessInfoCookie = (() => {
-  const accessInfo = cookies.split(';').find((item) => {
-    const [key] = item.split('=').map((part) => part.trim());
-    return key === 'accessInfo';
-  });
+	const accessInfo = cookies.split(';').find((item) => {
+		const [key] = item.split('=').map((part) => part.trim());
+		return key === 'accessInfo';
+	});
 
-  if(accessInfo) {
-    const value = accessInfo.split('=')[1].trim();
-    return JSON.parse(atob(value.split('.')[1].replace(/-/g, '+').replace(/_/g, '/')));
-  }
+	if (accessInfo) {
+		const value = accessInfo.split('=')[1].trim();
+		return JSON.parse(atob(value.split('.')[1].replace(/-/g, '+').replace(/_/g, '/')));
+	}
 })();
+
+const urlParamsObj = new URLSearchParams(window.location.search);
+const isGiftArticle = Object.keys(Object.fromEntries(urlParamsObj)).includes('giftToken');
 
 const isPlusSubscriber = accessInfoCookie?.access.Spplus === true;
 
 export const subscriptionCookie = {
-  isPlusSubscriber
+	isPlusSubscriber,
+  isGiftArticle,
+  isOpen: isPlusSubscriber || isGiftArticle,
 };
