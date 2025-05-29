a525ff4c0bb5c270b73e93391ad2cbea27d20409
Matthias Stahl <matthias.stahl87@web.de>
2025-05-29 14:19:01 +0200

    documentation

--- subscriptionCookie.js
+++ subscriptionCookie.js
@@ -1,10 +1,11 @@
 /**
  * @module subscriptionCookie
  * 
- * This module handles the extraction of a subscription status. 
+ * This module handles the extraction of a subscription or gift article status. 
  * Retrieves and parses the 'accessInfo' cookie to determine subscription status.
+ * Reads the URL parameters to check if the article is a gift article.
  * 
- * subscriptionCookie.isPlusSubscriber - Indicates if the user is a Plus subscriber.
+ * Returns an object with the following properties:
 
  * isPlusSubscriber (boolean) - Indicates if the user is a Plus subscriber based on the 'accessInfo' cookie.
  * isGiftArticle (boolean) - Indicates if the article was called as a gift article (gift token is not validated).
@@ -14,24 +15,28 @@
 const cookies = document.cookie;
 
 const accessInfoCookie = (() => {
-	const accessInfo = cookies.split(';').find((item) => {
-		const [key] = item.split('=').map((part) => part.trim());
-		return key === 'accessInfo';
-	});
+  const accessInfo = cookies.split(";").find((item) => {
+    const [key] = item.split("=").map((part) => part.trim());
+    return key === "accessInfo";
+  });
 
-	if (accessInfo) {
-		const value = accessInfo.split('=')[1].trim();
-		return JSON.parse(atob(value.split('.')[1].replace(/-/g, '+').replace(/_/g, '/')));
-	}
+  if (accessInfo) {
+    const value = accessInfo.split("=")[1].trim();
+    return JSON.parse(
+      atob(value.split(".")[1].replace(/-/g, "+").replace(/_/g, "/"))
+    );
+  }
 })();
 
 const urlParamsObj = new URLSearchParams(window.location.search);
-const isGiftArticle = Object.keys(Object.fromEntries(urlParamsObj)).includes('giftToken');
+const isGiftArticle = Object.keys(Object.fromEntries(urlParamsObj)).includes(
+  "giftToken"
+);
 
 const isPlusSubscriber = accessInfoCookie?.access.Spplus === true;
 
 export const subscriptionCookie = {
-	isPlusSubscriber,
+  isPlusSubscriber,
   isGiftArticle,
   isOpen: isPlusSubscriber || isGiftArticle,
 };
